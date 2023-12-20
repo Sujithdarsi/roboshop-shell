2c0d8bccb54d3c945ff1d4ae6ee56d17e2511fdb
Sujithdarsi <sujithdarsi.ucen@gmail.com>
2023-12-20 13:17:33 +0530

    added script

---
+++ web.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+    then
+        echo -e " $2 ... $R Installation Unsuccessful $N "
+        exit 1
+    else
+        echo -e " $2 ... $G Installion Successful $N "
+    fi
+
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e " $R Error : You are not root user,please run by root user $N "
+    exit 1
+else
+    echo -e " $G You are root user $N "
+fi
+
+dnf install nginx -y &>> $LOGFILE
+
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx &>> $LOGFILE
+
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx &>> $LOGFILE
+
+VALIDATE $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/* &>> $LOGFILE
+
+VALIDATE $? "Removing default content"
+
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> $LOGFILE
+
+VALIDATE $? "Downloading content"
+
+cd /usr/share/nginx/html &>> $LOGFILE
+
+VALIDATE $? "Extracting content"
+
+unzip -o /tmp/web.zip &>> $LOGFILE
+
+VALIDATE $? "Unzipping content"
+
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>> $LOGFILE
+
+VALIDATE $? "Copying roboshop config"
