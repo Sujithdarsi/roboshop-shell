0beaea1439f8c8fa20b27b989377223110a1f5ae
Sujithdarsi <sujithdarsi.ucen@gmail.com>
2023-12-20 13:25:52 +0530

    added script

--- web.sh
+++ web.sh
@@ -58,7 +58,7 @@ unzip -o /tmp/web.zip &>> $LOGFILE
 
 VALIDATE $? "Unzipping content"
 
-cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>> $LOGFILE
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf 
 
 VALIDATE $? "Copying roboshop config"
 
