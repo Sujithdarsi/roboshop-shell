5feaead1e4812a48e417cf9dd962d41b41fdef46
Sujithdarsi <sujithdarsi.ucen@gmail.com>
2023-12-20 09:06:55 +0530

    added script

--- catalogue.sh
+++ catalogue.sh
@@ -3,6 +3,8 @@
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
+MONGO_HOST=mongodb.daws23.online
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
@@ -15,6 +17,7 @@ VALIDATE(){
     if [ $1 -ne 0 ]
     then
         echo -e " $2 ... $R Installation Unsuccessful $N "
+        exit 1
     else
         echo -e " $2 ... $G Installion Successful $N "
     fi
@@ -29,17 +32,66 @@ else
     echo -e " $G You are root user $N "
 fi
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>> $LOGFILE
 
 VALIDATE $? "nodejs disabling"
 
-dnf module enable nodejs:18 -y
+dnf module enable nodejs:18 -y &>> $LOGFILE
 
 VALIDATE $? "Enabling nodejs:18"
 
-dnf install nodejs -y
+dnf install nodejs -y &>> $LOGFILE
 
 VALIDATE $? "Installing nodejs"
+ 
+useradd roboshop &>> $LOGFILE
 
-useradd roboshop
+VALIDATE $? "Adding user to roboshop"
 
+mkdir /app &>> $LOGFILE
+
+VALIDATE $? "Creating directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+ 
+VALIDATE $? "Zipping roboshop catalogue"
+
+cd /app 
+
+unzip /tmp/catalogue.zip &>> $LOGFILE
+
+VALIDATE $? "Unzipping roboshop catalogue"
+
+cd /app
+
+npm install &>> $LOGFILE
+
+VALIDATE $? "Installing packages"
+
+cp /home/centos/roboshop-shell/catalogue.service /home/centos/roboshop-shell &>> $LOGFILE
+
+VALIDATE $? "Adding data to catalogue.service"
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "Reloading the daemon"
+
+systemctl enable catalogue &>> $LOGFILE
+
+VALIDATE $? "Enabling catalogue"
+
+systemctl start catalogue &>> $LOGFILE
+
+VALIDATE $? "Starting catalogue"
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "Copying mongodb repo "
+
+dnf install mongodb-org-shell -y &>> $LOGFILE
+
+VALIDATE $? "Installing mongodb client"
+
+mongo --host $MONGO_HOST </app/schema/catalogue.js &>> $LOGFILE
+ 
+VALIDATE $? "Adding mongodb host address to catalogue "
