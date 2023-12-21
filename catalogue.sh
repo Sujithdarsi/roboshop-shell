09cba2582368aa10efff9b0d830b4c0056544cab
Sujithdarsi <sujithdarsi.ucen@gmail.com>
2023-12-21 11:33:18 +0530

    added script

--- catalogue.sh
+++ catalogue.sh
@@ -9,6 +9,8 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+MONGO_HOST=mongodb.daws23.online
+
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
@@ -96,6 +98,6 @@ dnf install mongodb-org-shell -y &>> $LOGFILE
 
 VALIDATE $? "Installing mongodb client"
 
-mongo --host mongodb.daws23.online </app/schema/catalogue.js &>> $LOGFILE
+mongo --host $MONGO_HOST </app/schema/catalogue.js &>> $LOGFILE
  
 VALIDATE $? "Adding mongodb host address to catalogue "
